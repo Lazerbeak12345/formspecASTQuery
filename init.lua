cc301d6622225576108e0c4b46a1bf8183874009
Nathan Fritzler <nfblaster@live.com>
2023-04-20 10:10:40 -0600

    feat(__index)!: support getting wrapped children
    BREAKING CHANGE: code depending on children being raw instead of wrapped
    may not function exactly the same

--- init.lua
+++ init.lua
@@ -36,7 +36,31 @@ function Qmt:_rawForEach()
 		return i - 1, self:_resolve_path(value)
 	end
 end
+local function add_to_path(path, index)
+	if path == true then
+		return { index }
+	end
+	local new_path = {}
+	for i, v in ipairs(path) do
+		new_path[i] = v
+	end
+	new_path[#new_path+1] = index
+	return new_path
+end
+function Qmt:getAllChildrenAtIndex(index)
+	local paths = {}
+	for path_index, elm in self:_rawForEach() do
+		local path = self._paths[path_index]
+		if elm[index] then
+			paths[#paths+1] = add_to_path(path, index)
+		end
+	end
+	return constructor{ _raw = self._raw, _paths = paths }
+end
 function Qmt:__index(key)
+	if type(key) == "number" then
+		return self:getAllChildrenAtIndex(key)
+	end
 	return Qmt[key] or self:_resolve_path(self._paths[1])[key]
 end
 function Qmt:__newindex(key, value)
@@ -70,14 +94,11 @@ function Qmt:findAll(needle)
 	local paths = {}
 	local function recurse(path, tree)
 		for index, elm in ipairs(tree) do
-			local new_path = {}
-			if path ~= true then
-				for i, v in ipairs(path) do
-					new_path[i] = v
-				end
-			end
-			new_path[#new_path+1] = index
-			if needle(elm) then
+			local new_path = add_to_path(path, index)
+			if needle(constructor{
+				_raw = self._raw,
+				_paths = { new_path }
+			}) then
 				paths[#paths+1] = new_path
 			end
 			recurse(new_path, elm)
--- spec/fASTQuery_spec.lua
+++ spec/fASTQuery_spec.lua
@@ -26,4 +26,22 @@ describe("wrapper", function ()
 			{ type = "box", color = "#000000" },
 		}, "modified only the colors")
 	end)
+	it("supports getting children, and children are also wrapped", function ()
+		local dom = {
+			type = "container",
+			{ type = "box", color = "#FFFFFF" }
+		}
+		local wrapped_child = Q(dom)[1]
+		assert.truthy(wrapped_child, "child found")
+		assert.equal(wrapped_child.type, dom[1].type, "child type is the same")
+		assert.equal(wrapped_child.color, dom[1].color, "child color is the same")
+		assert.equal(#wrapped_child, #dom[1], "child length is the same")
+		assert.truthy(wrapped_child.findAll, "child is wrapped")
+		wrapped_child.color = "#000000"
+		assert.equal(wrapped_child.color, dom[1].color, "child color is still the same")
+		assert.same(dom, {
+			type = "container",
+			{ type = "box", color = "#000000" }
+		}, "child was modified")
+	end)
 end)
