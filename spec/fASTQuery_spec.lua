5e315a0183c5d8cc80499b0eeb94cccf40857a5a
Nathan Fritzler <nfblaster@live.com>
2023-04-27 20:25:19 -0600

    test: break down components into 3 categories

--- spec/fASTQuery_spec.lua
+++ spec/fASTQuery_spec.lua
@@ -42,140 +42,144 @@ describe("wrapper", function ()
 		assert.equal(#Q(dom), 3)
 	end)
 end)
-describe("all", function ()
-	it("can search by table", function ()
-		local dom = { type = "container", { type = "box", color = "#FFFFFF" }, { type = "box", color = "#aFFFFF" } }
-		Q(dom):all{ type = "box" }.color = "#000000"
-		assert.same(dom, { type = "container", { type = "box", color = "#000000" }, { type = "box", color = "#000000" } })
+describe("navigation", function ()
+	describe("all", function ()
+		it("can search by table", function ()
+			local dom = { type = "container", { type = "box", color = "#FFFFFF" }, { type = "box", color = "#aFFFFF" } }
+			Q(dom):all{ type = "box" }.color = "#000000"
+			assert.same(dom, { type = "container", { type = "box", color = "#000000" }, { type = "box", color = "#000000" } })
+		end)
+	end)
+	describe("first", function ()
+		it("only finds one element", function ()
+			local dom = { type = "container", { type = "box", color = "#FFFFFF" }, { type = "box", color = "#aFFFFF" } }
+			Q(dom):first{ type = "box" }.color = "#000000"
+			assert.same(dom, { type = "container", { type = "box", color = "#000000" }, { type = "box", color = "#aFFFFF" } })
+		end)
+		it("gives the first element when no args", function ()
+			local dom = { type = "container", { type = "box", color = "#FFFFFF" }, { type = "box", color = "#aFFFFF" } }
+			-- Remember, all is slow. This is bad. Don't do this, just do :first(pattern). Reduce first, then pattern
+			Q(dom):all{ type = "box" }:first().color = "#000000"
+			assert.same(dom, { type = "container", { type = "box", color = "#000000" }, { type = "box", color = "#aFFFFF" } })
+		end)
+		it("provides a way to tell if none were found", function ()
+			local dom = { type = "container", { type = "label", label = "lol" } }
+			assert.equal(Q(dom):first{ type = "box" }:count(), 0, "none should be found")
+		end)
+	end)
+	describe("children", function ()
+		it("finds all children of all selected elements", function ()
+			local dom = {
+				type = "container",
+				{ type = "container", findMe = "pls", { type = "box", color = "#FFFFFF" }, { type = "box", color = "#aFFFFF" } },
+				{ type = "box", color = "#bFFFFF" },
+				{ type = "container", findMe = "pls", { type = "box", color = "#cFFFFF" }, { type = "box", color = "#dFFFFF" } }
+			}
+			Q(dom):all{ findMe = "pls" }:children().color = "#000000"
+			assert.same(dom, {
+				type = "container",
+				{ type = "container", findMe = "pls", { type = "box", color = "#000000" }, { type = "box", color = "#000000" } },
+				{ type = "box", color = "#bFFFFF" },
+				{ type = "container", findMe = "pls", { type = "box", color = "#000000" }, { type = "box", color = "#000000" } }
+			})
+		end)
+	end)
+	describe("include", function ()
+		it("adds elements found in a different search into this search", function ()
+			local dom = {
+				type = "container",
+				{ type = "box", color = "#FFFFFF" },
+				{ type = "box", color = "#cFFFFF" },
+				{ type = "container", { type = "box", color = "#aFFFFF" }, { type = "box", color = "#bFFFFF" } }
+			}
+			local first = Q(dom):first{ type = "box" }
+			Q(dom)[3]:all{ type = "box" }:include(first).color = "#000000"
+			assert.same(dom, {
+				type = "container",
+				{ type = "box", color = "#000000" },
+				{ type = "box", color = "#cFFFFF" },
+				{ type = "container", { type = "box", color = "#000000" }, { type = "box", color = "#000000" } }
+			})
+		end)
+	end)
+	describe("count", function ()
+		it("counts the number of matches made", function ()
+			local dom = {
+				type = "container",
+				{ type = "box", color = "#FFFFFF" },
+				{ type = "label", label = "hi"},
+				{ type = "box", color = "#FFFFFF" },
+				{ type = "box", color = "#FFFFFF" }
+			}
+			local container = Q(dom)
+			assert.equal(container:all{ type = "box" }:count(), 3, "there are three matches")
+			assert.equal(container:first{ type = "box" }:count(), 1, "first found only just the one")
+		end)
+	end)
+	describe("each", function ()
+		it("is an iterator returning wrapped elements", function ()
+			local dom = {
+				type = "container",
+				{ type = "container", findMe = "pls", { type = "box", color = "#FFFFFF" }, { type = "box", color = "#aFFFFF" } },
+				{ type = "box", color = "#bFFFFF" },
+				{ type = "container", findMe = "pls", { type = "box", color = "#cFFFFF" }, { type = "box", color = "#dFFFFF" } }
+			}
+			for elm in Q(dom):all{ findMe = "pls" }:children():each() do
+				elm.color = "#000000"
+				assert.is.truthy(elm.each, "is wrapped")
+			end
+			assert.same(dom, {
+				type = "container",
+				{ type = "container", findMe = "pls", { type = "box", color = "#000000" }, { type = "box", color = "#000000" } },
+				{ type = "box", color = "#bFFFFF" },
+				{ type = "container", findMe = "pls", { type = "box", color = "#000000" }, { type = "box", color = "#000000" } }
+			})
+		end)
+	end)
+	describe("parents", function ()
+		it("returns the parents of each element", function ()
+			local dom = {
+				type = "container",
+				{ type = "container", { type = "box", color = "#FFFFFF" } },
+				{ type = "container", { type = "box", color = "#FFFFFF" } }
+			}
+			Q(dom):all{ type = "box" }:parents().type = "vbox"
+			assert.same(dom, {
+				type = "container",
+				{ type = "vbox", { type = "box", color = "#FFFFFF" } },
+				{ type = "vbox", { type = "box", color = "#FFFFFF" } }
+			})
+		end)
+		it("does not match duplicates", function ()
+			local dom = {
+				type = "container",
+				{ type = "container", { type = "box", color = "#FFFFFF" }, { type = "box", color = "#FFFFFF" } }
+			}
+			local count = 0
+			for _ in Q(dom):all{ type = "box" }:parents():each() do
+				count = count + 1
+			end
+			assert.equal(1, count)
+		end)
 	end)
 end)
-describe("first", function ()
-	it("only finds one element", function ()
-		local dom = { type = "container", { type = "box", color = "#FFFFFF" }, { type = "box", color = "#aFFFFF" } }
-		Q(dom):first{ type = "box" }.color = "#000000"
-		assert.same(dom, { type = "container", { type = "box", color = "#000000" }, { type = "box", color = "#aFFFFF" } })
-	end)
-	it("gives the first element when no args", function ()
-		local dom = { type = "container", { type = "box", color = "#FFFFFF" }, { type = "box", color = "#aFFFFF" } }
-		-- Remember, all is slow. This is bad. Don't do this, just do :first(pattern). Reduce first, then pattern
-		Q(dom):all{ type = "box" }:first().color = "#000000"
-		assert.same(dom, { type = "container", { type = "box", color = "#000000" }, { type = "box", color = "#aFFFFF" } })
-	end)
-	it("provides a way to tell if none were found", function ()
-		local dom = { type = "container", { type = "label", label = "lol" } }
-		assert.equal(Q(dom):first{ type = "box" }:count(), 0, "none should be found")
-	end)
-end)
-describe("children", function ()
-	it("finds all children of all selected elements", function ()
-		local dom = {
-			type = "container",
-			{ type = "container", findMe = "pls", { type = "box", color = "#FFFFFF" }, { type = "box", color = "#aFFFFF" } },
-			{ type = "box", color = "#bFFFFF" },
-			{ type = "container", findMe = "pls", { type = "box", color = "#cFFFFF" }, { type = "box", color = "#dFFFFF" } }
-		}
-		Q(dom):all{ findMe = "pls" }:children().color = "#000000"
-		assert.same(dom, {
-			type = "container",
-			{ type = "container", findMe = "pls", { type = "box", color = "#000000" }, { type = "box", color = "#000000" } },
-			{ type = "box", color = "#bFFFFF" },
-			{ type = "container", findMe = "pls", { type = "box", color = "#000000" }, { type = "box", color = "#000000" } }
-		})
-	end)
-end)
-describe("include", function ()
-	it("adds elements found in a different search into this search", function ()
-		local dom = {
-			type = "container",
-			{ type = "box", color = "#FFFFFF" },
-			{ type = "box", color = "#cFFFFF" },
-			{ type = "container", { type = "box", color = "#aFFFFF" }, { type = "box", color = "#bFFFFF" } }
-		}
-		local first = Q(dom):first{ type = "box" }
-		Q(dom)[3]:all{ type = "box" }:include(first).color = "#000000"
-		assert.same(dom, {
-			type = "container",
-			{ type = "box", color = "#000000" },
-			{ type = "box", color = "#cFFFFF" },
-			{ type = "container", { type = "box", color = "#000000" }, { type = "box", color = "#000000" } }
-		})
-	end)
-end)
-describe("getKey", function ()
-	it("gets a key, regardless of if that key is shadowed by this api", function ()
-		local dom = { type = "box", getKey = 3 }
-		assert.equal(Q(dom):getKey("getKey"), 3)
-	end)
-	it("only returns the first key", function()
-		local dom = {
-			type = "container",
-			{ type = "box", color = "#FFFFFF" },
-			{ type = "box", color = "#aFFFFF" }
-		}
-		assert.equal(Q(dom):children():getKey("color"), "#FFFFFF")
-	end)
-	it("is an unalias for just getting the key conventionally", function ()
-		local dom = { type = "container", { type = "box", color = "#FFFFFF" }, { type = "box", color = "#aFFFFF" } }
-		assert.equal(Q(dom):children().color, "#FFFFFF")
-	end)
-end)
-describe("count", function ()
-	it("counts the number of matches made", function ()
-		local dom = {
-			type = "container",
-			{ type = "box", color = "#FFFFFF" },
-			{ type = "label", label = "hi"},
-			{ type = "box", color = "#FFFFFF" },
-			{ type = "box", color = "#FFFFFF" }
-		}
-		local container = Q(dom)
-		assert.equal(container:all{ type = "box" }:count(), 3, "there are three matches")
-		assert.equal(container:first{ type = "box" }:count(), 1, "first found only just the one")
-	end)
-end)
-describe("each", function ()
-	it("is an iterator returning wrapped elements", function ()
-		local dom = {
-			type = "container",
-			{ type = "container", findMe = "pls", { type = "box", color = "#FFFFFF" }, { type = "box", color = "#aFFFFF" } },
-			{ type = "box", color = "#bFFFFF" },
-			{ type = "container", findMe = "pls", { type = "box", color = "#cFFFFF" }, { type = "box", color = "#dFFFFF" } }
-		}
-		for elm in Q(dom):all{ findMe = "pls" }:children():each() do
-			elm.color = "#000000"
-			assert.is.truthy(elm.each, "is wrapped")
-		end
-		assert.same(dom, {
-			type = "container",
-			{ type = "container", findMe = "pls", { type = "box", color = "#000000" }, { type = "box", color = "#000000" } },
-			{ type = "box", color = "#bFFFFF" },
-			{ type = "container", findMe = "pls", { type = "box", color = "#000000" }, { type = "box", color = "#000000" } }
-		})
-	end)
-end)
-describe("parents", function ()
-	it("returns the parents of each element", function ()
-		local dom = {
-			type = "container",
-			{ type = "container", { type = "box", color = "#FFFFFF" } },
-			{ type = "container", { type = "box", color = "#FFFFFF" } }
-		}
-		Q(dom):all{ type = "box" }:parents().type = "vbox"
-		assert.same(dom, {
-			type = "container",
-			{ type = "vbox", { type = "box", color = "#FFFFFF" } },
-			{ type = "vbox", { type = "box", color = "#FFFFFF" } }
-		})
-	end)
-	it("does not match duplicates", function ()
-		local dom = {
-			type = "container",
-			{ type = "container", { type = "box", color = "#FFFFFF" }, { type = "box", color = "#FFFFFF" } }
-		}
-		local count = 0
-		for _ in Q(dom):all{ type = "box" }:parents():each() do
-			count = count + 1
-		end
-		assert.equal(1, count)
+describe("manipulation", function ()
+	describe("getKey", function ()
+		it("gets a key, regardless of if that key is shadowed by this api", function ()
+			local dom = { type = "box", getKey = 3 }
+			assert.equal(Q(dom):getKey("getKey"), 3)
+		end)
+		it("only returns the first key", function()
+			local dom = {
+				type = "container",
+				{ type = "box", color = "#FFFFFF" },
+				{ type = "box", color = "#aFFFFF" }
+			}
+			assert.equal(Q(dom):children():getKey("color"), "#FFFFFF")
+		end)
+		it("is an unalias for just getting the key conventionally", function ()
+			local dom = { type = "container", { type = "box", color = "#FFFFFF" }, { type = "box", color = "#aFFFFF" } }
+			assert.equal(Q(dom):children().color, "#FFFFFF")
+		end)
 	end)
 end)
